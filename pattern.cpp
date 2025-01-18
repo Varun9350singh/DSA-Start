da16b3019c8ffa27c447b6a6dd7af2e504413458
varun singh <77410003+Varun9350singh@users.noreply.github.com>
2025-01-18 22:24:17 +0530

    first pattern

--- pattern.cpp
+++ pattern.cpp
@@ -1,6 +1,14 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    
 
+int main() 
+{
+  int n;
+  cin>>n;
+  for(int i=0;i<n;i++){
+    for(int j=0;j<n;j++){
+      cout<<" 0";
+    }
+    cout<<endl;
+  }
 }
